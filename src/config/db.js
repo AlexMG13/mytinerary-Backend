919e1bf3e3dcdf12c945f420973da60d4c3b6078
Alex <mendozaalex24@gmail.com>
2023-08-22 15:05:45 -0300

    env archive created

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,6 @@
 const { connect } = require('mongoose')
+require('dotenv').config()
 
-const URL = 'mongodb+srv://mendozaalex24:2cQlGHHdhCdnCrh0@cluster0.zb0uogo.mongodb.net/?retryWrites=true&w=majority'
-
-
-
-connect(URL)
+connect(process.env.MONGO)
     .then( () => {console.log('connect success to database')})
     .catch( () => {console.log('error connecting to database')})
